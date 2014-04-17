a266a79f9e70ed02642240d97b61c49a67e790c9
Jia Xu <dolremi@gmail.com>
2014-04-16 23:49:59 -0400

    add NeuralTrainer class

---
+++ neuraltrainer.cpp
@@ -0,0 +1,14 @@
+/*
+ * neuraltrainer.cpp
+ *
+ *  Created on: Apr 16, 2014
+ *      Author: debian
+ */
+
+#include "neuraltrainer.h"
+
+NeuralTrainer::NeuralTrainer() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ neuraltrainer.h
@@ -0,0 +1,16 @@
+/*
+ * neuraltrainer.h
+ *
+ *  Created on: Apr 16, 2014
+ *      Author: debian
+ */
+
+#ifndef NEURALTRAINER_H_
+#define NEURALTRAINER_H_
+
+class NeuralTrainer {
+public:
+	NeuralTrainer();
+};
+
+#endif /* NEURALTRAINER_H_ */
